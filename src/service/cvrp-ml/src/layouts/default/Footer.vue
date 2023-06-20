9c5b7aea5c893476fb74028008d53e69c045276c
Germano Barcelos <gegenbarcelos@gmail.com>
2023-06-20 01:39:34 -0300

    add web

---
+++ src/service/cvrp-ml/src/layouts/default/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer class="bg-teal">
+    <v-row justify="center" no-gutters>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Germano Barcelos</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
